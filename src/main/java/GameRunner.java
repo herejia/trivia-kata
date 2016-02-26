89c3b42a3a46fdfc9e4681abbbcb500fd9c7ab43
herezia <william.huart@gmail.com>
2016-02-26 22:07:22 +0100

    indent and format

--- src/main/java/GameRunner.java
+++ src/main/java/GameRunner.java
@@ -3,31 +3,30 @@ import java.util.Random;
 
 public class GameRunner {
 
-	public static final int SEED = 42;
-	private static boolean notAWinner;
+    public static final int SEED = 42;
+    private static boolean notAWinner;
 
-	public static void main(String[] args) {
-		Game aGame = new Game();
-		
-		aGame.add("Chet");
-		aGame.add("Pat");
-		aGame.add("Sue");
-		
-		Random rand = new Random(SEED);
-	
-		do {
-			
-			aGame.roll(rand.nextInt(5) + 1);
-			
-			if (rand.nextInt(9) == 7) {
-				notAWinner = aGame.wrongAnswer();
-			} else {
-				notAWinner = aGame.wasCorrectlyAnswered();
-			}
-			
-			
-			
-		} while (notAWinner);
-		
-	}
+    public static void main(String[] args) {
+        Game aGame = new Game();
+
+        aGame.add("Chet");
+        aGame.add("Pat");
+        aGame.add("Sue");
+
+        Random rand = new Random(SEED);
+
+        do {
+
+            aGame.roll(rand.nextInt(5) + 1);
+
+            if (rand.nextInt(9) == 7) {
+                notAWinner = aGame.wrongAnswer();
+            } else {
+                notAWinner = aGame.wasCorrectlyAnswered();
+            }
+
+
+        } while (notAWinner);
+
+    }
 }
